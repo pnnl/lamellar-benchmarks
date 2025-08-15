c25b84a951f1885c62dbe363ba6e19a3f11563cd
Joseph A Cottam <joseph.cottam@pnnl.gov>
2025-08-15 11:58:43 -0700

    Fixing deadlocks due ot missing  calls

--- histo/src/histo_lamellar_atomicarray.rs
+++ histo/src/histo_lamellar_atomicarray.rs
@@ -15,7 +15,7 @@ fn histo(counts: &AtomicArray<usize>, rand_index: &ReadOnlyArray<usize>) {
 //===== HISTO END ======
 
 const COUNTS_LOCAL_LEN: usize = 1000000; //100_000_000; //this will be 800MB on each pe
-                                         // srun -N <num nodes> target/release/histo_lamellar_array <num updates>
+                                         // srun --cpu-bind=ldoms --mpi=pmi2 --cpus-per-task=4 --tasks-per-node=2  -N 2 target/debug/histo_lamellar_array_comparison 2
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let world = lamellar::LamellarWorldBuilder::new().build();
@@ -39,6 +39,7 @@ fn main() {
         lamellar::array::Distribution::Cyclic,
     );
 
+
     let rand_index = UnsafeArray::<usize>::new(
         world.team(),
         l_num_updates * num_pes,
@@ -49,7 +50,7 @@ fn main() {
     let unsafe_counts = unsafe_counts.block();
 
     // initialize arrays
-    let counts_init = unsafe { unsafe_counts.dist_iter_mut().for_each(|x| *x = 0) };
+    let counts_init = unsafe { unsafe_counts.dist_iter_mut().for_each(|x| *x = 0).spawn() };
     // rand_index.dist_iter_mut().for_each(move |x| *x = rng.lock().gen_range(0,global_count)).wait(); //this is slow because of the lock on the rng so we will do unsafe slice version instead...
 
     let rand_index = rand_index.block();
@@ -60,6 +61,7 @@ fn main() {
         }
     }
     world.block_on(counts_init);
+
     let counts = unsafe_counts.into_atomic().block();
     //counts.wait_all(); equivalent in this case to the above statement
     let rand_index = rand_index.into_read_only().block();
@@ -71,6 +73,7 @@ fn main() {
         println!("{:?} issue time {:?} ", my_pe, now.elapsed());
     }
     counts.wait_all();
+
     if my_pe == 0 {
         println!(
             "local run time {:?} local mups: {:?}",
@@ -79,6 +82,8 @@ fn main() {
         );
     }
     counts.barrier();
+    println!("My pe {my_pe}: Counts awaited", );
+
     let global_time = now.elapsed().as_secs_f64();
     if my_pe == 0 {
         println!(
